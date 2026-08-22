e3c9d01daf4feca9a5ecdd1667dd317b9c629b21
Jeet1466 <j33t2006@gmail.com>
2026-08-22 12:04:42 +0530

    Q1

--- Q1/main.sql
+++ Q1/main.sql
@@ -5,31 +5,55 @@ CREATE OR REPLACE PROCEDURE REGISTER_STUDENT(
     p_SubjectID IN Int
 )
 IS
-    v_studentID int;
+    v_studentID 
+    int;
     v_subjectID int;
     V_COUNT int;
     v_seats int;
+    Student_Dont_Exists EXCEPTION;
+    Subject_Dont_Exists EXCEPTION;
+    Student_Already_Registered EXCEPTION;
+    Seats_Not_Available EXCEPTION;
 BEGIN
-    SELECT StudentID INTO v_studentID FROM Student WHERE StudentID = p_StudentID;
-    SELECT SubjectID, Seats INTO v_subjectID, v_seats FROM Subject WHERE SubjectID = p_SubjectID;
+    SELECT COUNT(*) INTO v_studentID FROM Student WHERE StudentID = p_StudentID;
+    SELECT COUNT(*) INTO v_subjectID FROM Subject WHERE SubjectID = p_SubjectID;
     SELECT COUNT(*)INTO v_count FROM Registration WHERE StudentID = p_StudentID AND SubjectID = p_SubjectID;
 
-    IF v_studentID IS NULL THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Student does not exist.');
-    ELSIF v_subjectID IS NULL THEN
-        RAISE_APPLICATION_ERROR(-20002, 'Subject does not exist.');
+    IF v_studentID = 0 THEN
+        RAISE Student_Dont_Exists;
+    ELSIF v_subjectID = 0 THEN
+        RAISE Subject_Dont_Exists;
     ELSIF v_count > 0 THEN
-        RAISE_APPLICATION_ERROR(-20004, 'Student is already registered for this subject.'); 
-    ELSIF v_seats <= 0 THEN
-        RAISE_APPLICATION_ERROR(-20003, 'No seats available for the subject.');
+        RAISE Student_Already_Registered;
     ELSE
-        INSERT INTO Registration (RegID, StudentID, SubjectID, RegDate)
-        VALUES (Registration_seq.NEXTVAL, p_StudentID, p_SubjectID, SYSDATE);
+        SELECT Seats INTO v_seats FROM Subject WHERE SubjectID = p_SubjectID;
+        IF v_seats <= 0 THEN
+            RAISE Seats_Not_Available;
+        ELSE
 
-        UPDATE Subject SET Seats = Seats - 1 WHERE SubjectID = p_SubjectID;
+            INSERT INTO Registration (RegID, StudentID, SubjectID, RegDate)
+            VALUES (Registration_seq.NEXTVAL, p_StudentID, p_SubjectID, SYSDATE);
 
-        INSERT INTO Student_Audit (AuditID, ActionType, ActionDate)
-        VALUES (Student_Audit_seq.NEXTVAL, 'REGISTER', SYSDATE);
+            UPDATE Subject SET Seats = Seats - 1 WHERE SubjectID = p_SubjectID;
+
+            INSERT INTO Student_Audit (AuditID, ActionType, ActionDate)
+            VALUES (Student_Audit_seq.NEXTVAL, 'REGISTER', SYSDATE);
+
+            DBMS_OUTPUT.PUT_LINE('Student registered successfully.');
+        END IF;
     END IF;
+
+EXCEPTION
+    WHEN Student_Dont_Exists THEN
+        DBMS_OUTPUT.PUT_LINE('Student does not exist.');
+    WHEN Subject_Dont_Exists THEN
+        DBMS_OUTPUT.PUT_LINE('Subject does not exist.');
+    WHEN Student_Already_Registered THEN
+        DBMS_OUTPUT.PUT_LINE('Student is already registered for this subject.');
+    WHEN Seats_Not_Available THEN
+        DBMS_OUTPUT.PUT_LINE('No available seats for the subject.');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('An unexpected error occurred: ' || SQLERRM);
+    
 END;
 /
